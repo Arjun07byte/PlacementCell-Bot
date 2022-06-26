ee475e29e409276b4fc7223611f06e5fb363bd23
OnlyArjun <91238510+OnlyArjun@users.noreply.github.com>
2022-06-26 12:25:17 +0530

    Delete constants.py

--- constants.py
+++
@@ -1,3 +0,0 @@
-bot_api = "5571200685:AAFEyoTiZm7RyGPu5mq7wWiqcU6tkGxjeN8"
-internship_URL = "https://pgdavplacementcell.in/internships.php"
-drives_URL = "https://pgdavplacementcell.in/registration.php"
