7bf17e839886f9dd5d41b3c80160055341e1a2f9
OnlyArjun <91238510+OnlyArjun@users.noreply.github.com>
2022-06-26 11:43:50 +0530

    constants_File

---
+++ constants.py
@@ -0,0 +1,3 @@
+bot_api = "5571200685:AAFEyoTiZm7RyGPu5mq7wWiqcU6tkGxjeN8"
+internship_URL = "https://pgdavplacementcell.in/internships.php"
+drives_URL = "https://pgdavplacementcell.in/registration.php"
